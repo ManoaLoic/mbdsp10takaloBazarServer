2c3de04489de2f85b464136881b08eb7c1c74e3b
Loïc <lorandrianarivelo@gmail.com>
2024-07-03 22:59:21 +0300

    Mes objets

--- controller/ObjectController.js
+++ controller/ObjectController.js
@@ -2,11 +2,11 @@ const ObjectRepository = require('../service/ObjectRepository');
 
 exports.getObjects = async (req, res) => {
     try {
-        let { page, limit, q } = req.query;
+        let { page, limit, q, userID } = req.query;
         page = page || "1";
         limit = limit || "50";
 
-        const { objects, totalPages, currentPage } = await ObjectRepository.getObjects(q, parseInt(page), parseInt(limit));
+        const { objects, totalPages, currentPage } = await ObjectRepository.getObjects(q, parseInt(page), parseInt(limit), userID);
 
         res.status(200).json({
             data: {
@@ -23,18 +23,18 @@ exports.getObjects = async (req, res) => {
     }
 };
 
-exports.removeObject = async (req,res) => {
+exports.removeObject = async (req, res) => {
     const { objectId } = req.params;
 
     try {
-      const updatedObject = await ObjectRepository.removeObject(objectId);
-      if (!updatedObject) {
-        return res.status(404).json({ error: 'Objet non trouvé' });
-      }
-      return res.status(200).json({ message: 'Statut mis à jour en \'removed\'', object: updatedObject });
+        const updatedObject = await ObjectRepository.removeObject(objectId);
+        if (!updatedObject) {
+            return res.status(404).json({ error: 'Objet non trouvé' });
+        }
+        return res.status(200).json({ message: 'Statut mis à jour en \'removed\'', object: updatedObject });
     } catch (error) {
-      console.error('Erreur lors de la mise à jour du statut de l\'objet:', error);
-      return res.status(500).json({ error: 'Erreur interne du serveur: '+ error.message });
+        console.error('Erreur lors de la mise à jour du statut de l\'objet:', error);
+        return res.status(500).json({ error: 'Erreur interne du serveur: ' + error.message });
     }
 }
 
@@ -42,10 +42,10 @@ exports.updateObject = async (req, res) => {
     try {
         const objectId = req.params.id;
         const data = req.body;
-        const updatedObject = await ObjectRepository.updateObject(objectId,data);
+        const updatedObject = await ObjectRepository.updateObject(objectId, data);
         res.status(200).json({
-            message : "SUCCESS",
-            data : updatedObject
+            message: "SUCCESS",
+            data: updatedObject
         });
     } catch (error) {
         res.status(500).json({ error: error.message });
