5c5062427693273d9f2ee14ac94d5fbe236b41d8
Jess <nomenjanaharimananajessica@gmail.com>
2024-07-04 08:55:22 +0300

    login BO [API]

--- controller/UserController.js
+++ controller/UserController.js
@@ -29,6 +29,28 @@ exports.loginUser = async (req, res) => {
   }
 };
 
+exports.loginAdmin = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({
+      error: "Le nom d'utilisateur ou l'email et le mot de passe sont requis",
+    });
+  }
+  try {
+    const admin = await UserRepository.login(username, password, "ADMIN");
+    if (!admin) {
+      return res.status(404).json({
+        error: "Nom d'utilisateur ou mot de passe incorrect. Veuillez réessayer.",
+      });
+    }
+    return res.status(200).json({ message: 'Connexion réussie', admin });
+  } catch (error) {
+    console.error("Erreur lors de la connexion:", error);
+    return res.status(500).json({ error: "Erreur interne du serveur: " + error.message });
+  }
+};
+
 exports.getUserProfile = async (req, res) => {
   try {
     const userProfile = await UserRepository.getUserProfile(req.params.userId);
