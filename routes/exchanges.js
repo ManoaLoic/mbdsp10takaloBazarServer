3fdc14c22f17df4e003e5eed102838f367c5f5c6
Jess <nomenjanaharimananajessica@gmail.com>
2024-07-09 01:32:44 +0300

    Mes échanges en cours

--- routes/exchanges.js
+++ routes/exchanges.js
@@ -4,4 +4,5 @@ const router = express.Router();
 const exchangeController = require('../controller/ExchangeController');
 
 router.get('/count', exchangeController.getCount);
+router.get('/myCurrents', exchangeController.getOpenExchanges);
 module.exports = router;
