94dad8d7b55f61cb1e297bac150ec3044ffeee10
10soa <soaornella5@gmail.com>
2024-07-09 20:43:01 +0300

    Update DashboardController.js

--- controller/DashboardController.js
+++ controller/DashboardController.js
@@ -2,21 +2,24 @@ const categoryRepository = require("../service/CategoryRepository");
 const ExchangeRepository = require("../service/ExchangeRepository");
 
 exports.getStatistics = async (req, res) => {
+    const { date1, date2, status } = req.query;
   try {
     const [
       ongoingExchanges,
       objectsByCategory,
+      exchangesBetweenDates,
       exchangesByUser,
     ] = await Promise.all([
       ExchangeRepository.getCountByStatus ('Proposed'),
       categoryRepository.getCategoryStatistics(),
+      ExchangeRepository.getExchangesBetweenDates(date1, date2, status),
       ExchangeRepository.getTopUsersByExchanges(),
     ]);
 
     return res.status(200).json({
       ongoingExchanges,
       objectsByCategory,
-      exchangesBetweenDates: null,
+      exchangesBetweenDates,
       exchangesByUser,
     });
   } catch (error) {
