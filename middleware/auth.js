78906327bc064036f53cbb5efff1d52427932e5d
Nick Mathieu <rakotomanananick29@gmail.com>
2024-07-09 20:53:00 +0300

    [APIs] Logout

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,16 @@
 const { expressjwt: jwt } = require('express-jwt');
-
+const jwt_decode = require('jsonwebtoken');
 const secretKey = process.env.SECRET_KEY;
+const revokedTokens = new Set();
 
 const authenticate = jwt({
     secret: secretKey,
     algorithms: ['HS256'],
     requestProperty: 'user',
+    isRevoked: (req, token) => {
+        const jti = token.payload.jti;
+        return revokedTokens.has(jti);
+    }
 });
 
 const authorize = (roles) => (req, res, next) => {
@@ -19,4 +24,5 @@ const authorize = (roles) => (req, res, next) => {
 module.exports = {
     authenticate,
     authorize,
+    revokedTokens
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,15 @@
 const express = require( 'express');
 const router = express.Router();
- 
+const { authenticate, revokedTokens } = require('../middleware/auth');
 const userController = require('../controller/UserController');
+const jwt = require('jsonwebtoken');
 
 router.post('/user/login', userController.loginUser);
 router.post('/admin/login', userController.loginAdmin);
-
+router.post('/logout', authenticate, (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.decode(token);
+    revokedTokens.add(decodedToken.jti);
+    res.status(200).json({ message: 'Logged out successfully' });
+});
 module.exports = router;
