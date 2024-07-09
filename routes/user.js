0416fbcfd02c47f47815b9bd0ea7e25d53845428
10soa <soaornella5@gmail.com>
2024-07-09 19:34:43 +0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const UserController = require('../controller/UserController');
-
+// 
 router.put('/:id', UserController.userUpdate);
 
 module.exports = router;
