932b13ab1fd35234d74e5a4498d5990ac9e7c990
ManoaLoic <lorandrianarivelo@gmail.com>
2024-08-18 23:38:23 +0300

    change

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-const DEFAULT_USER_PROFILE = 'https://storage.googleapis.com/takalobazaar.appspot.com/images/user/1723993705639_Emma_Emmanuella.png';
+const DEFAULT_USER_PROFILE = 'https://storage.googleapis.com/takalobazaar.appspot.com/images/user/1724013459572_Emma_Emmanuella.png';
 module.exports = { DEFAULT_USER_PROFILE };
